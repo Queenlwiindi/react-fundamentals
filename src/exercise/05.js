ea0fea4c3851b72792461a016c44f06abbb5b7f2
Queenlwiindi <queenlwiindi@gmail.com>
2022-09-30 10:54:01 +0200

    exercise 01 done

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -1,9 +1,6 @@
 // Styling
 // http://localhost:3000/isolated/exercise/05.js
 
-import * as React from 'react'
-import '../box-styles.css'
-
 // 🐨 add a className prop to each div and apply the correct class names
 // based on the text content
 // 💰 Here are the available class names: box, box--large, box--medium, box--small
@@ -13,19 +10,3 @@ import '../box-styles.css'
 // matches what the text says it should be
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
-
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
-
-function App() {
-  return (
-    <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
-    </div>
-  )
-}
-
-export default App
